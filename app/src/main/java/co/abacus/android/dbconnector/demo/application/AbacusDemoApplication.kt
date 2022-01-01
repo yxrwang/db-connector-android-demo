9b69cad8febf62ef998709933db5d742edc1d36f
Jack Wang <jack@meldcx.com>
2022-01-01 23:03:55 +1100

    Added image repo, added parcelable to data models

--- app/src/main/java/co/abacus/android/dbconnector/demo/application/AbacusDemoApplication.kt
+++ app/src/main/java/co/abacus/android/dbconnector/demo/application/AbacusDemoApplication.kt
@@ -16,4 +16,5 @@ class AbacusDemoApplication : Application() {
     fun getCategoriesRepo(): CategoriesRepository { return factory.categoriesRepo }
     fun getProductsRepo(): ProductsRepository { return factory.productsRepo }
     fun getModifiersRepo(): ModifiersRepository { return factory.modifiersRepo }
+    fun getImageRepo(): ImageRepository { return factory.imageRepository }
 }
